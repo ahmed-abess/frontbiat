c2aa3d8175d6c23773b97768a0220a17261b6440
abess_ahmed <ahmedbenabdallah1991@gmail.com>
2022-05-30 03:35:10 +0100

    second commit

--- src/Api.js
+++ src/Api.js
@@ -1,3 +1,3 @@
-export const API_HOST = "http://localhost:2525/";
+export const API_HOST = "http://localhost:8000/";
 export const API_LOGIN = "api/login_check";
 export const API_SIGNUP= "api/register";
